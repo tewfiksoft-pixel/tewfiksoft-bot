de6710811cd2e9ee0248027a08088432782cbdfc
TewfikSoft <tewfiksoft@gmail.com>
2026-05-30 11:13:59 +0100

    v10.3: BVA sequence number + config persistence fix

--- trigger_redeploy.js
+++ trigger_redeploy.js
@@ -1,2 +1,2 @@
-// Force redeploy - version v10.2 - (2026-05-26)
-console.log('Bot starting - TewfikSoft HR Bot v10.2...');
+// Force redeploy - version v10.3 - (2026-05-30)
+console.log('Bot starting - TewfikSoft HR Bot v10.3...');
